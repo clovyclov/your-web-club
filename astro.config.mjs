1d7fe5aac09431e3cff5dcb8b7fdba91c357868b
Clovis Shropshire <shropshireclovis@gmail.com>
2026-08-19 19:42:07 -0400

    Initial commit
    Set up Astro site with GitHub Pages deploy workflow.

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://clovyclov.github.io',
+  base: '/your-web-club',
+});
